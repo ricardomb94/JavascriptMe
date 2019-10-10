ea6d74baa1072d8074633c9dc377928171ae7ff7
ricardomb94 <aymardmb@gmail.com>
2019-10-10 07:49:21 +0200

    deuxième tentative d'affichage des données de saisie de formulaire

--- js/index.js
+++ js/index.js
@@ -1,107 +1,102 @@
 window.onload = function() {
-
     // --------------------- STEP 1 ---------------------
-        // Par defaut le formulaire de connection est afficher, le formulaire d'inscription quand a lui est en 'display: none';
-        // FAITE EN SORTE QUE AU CLICK SUR LES BUTTONS POSSEDANT LA CLASS 'square-button-empty'
-            // DE MASQUER LE LOGIN FORM POUR AFFICHER LE REGISTER FORM, ET INVERSEMENT <->
- 
+    // Par defaut le formulaire de connection est afficher, le formulaire d'inscription quand a lui est en 'display: none';
+    // FAITE EN SORTE QUE AU CLICK SUR LES BUTTONS POSSEDANT LA CLASS 'square-button-empty'
+    // DE MASQUER LE LOGIN FORM POUR AFFICHER LE REGISTER FORM, ET INVERSEMENT <->
 
-     const button = document.querySelectorAll('.square-button-empty');
 
-     for (let i = 0; i < button.length; i++) {
-        button[i].onclick = function(e){
-            if(e.target.getAttribute("data-form") == 0) {
-                document.getElementById("connexion-form").style.display = "none";
-                document.getElementById("register-form").style.display = "flex";
-            }
-            else{
-                document.getElementById("register-form").style.display = "none";
-                document.getElementById("connexion-form").style.display = "flex";
-                
-            }
+    const button = document.querySelectorAll('.square-button-empty');
 
-        }
-    }
+    for (let i = 0; i < button.length; i++) {
+       button[i].onclick = function(e){
+           if(e.target.getAttribute("data-form") == 0) {
+               document.getElementById("connexion-form").style.display = "flex";
+               document.getElementById("register-form").style.display = "none";
+           }
+           else if(e.target.getAttribute("data-form") == 1) {
+            document.getElementById("register-form").style.display = "flex";
+            document.getElementById("connexion-form").style.display = "none";
+           }
 
-    
+       }
+   }
 
 
 
-            
+
+
+
 
 
 
     // --------------------- STEP 2 ----------------------
-        // maintenant que l'on peut afficher nos 2 formulaires l'intéret serait maintenant qu'ils fonctionnent ! pour cela :
-        // FAITE EN SORTE QUE AU CLICK SUR LES BUTTONS POSSEDANT LA CLASS 'square-button' DE :
-            //  1. récuperer la valeur de tout les champs de formulaires
-            //  2. vérifier que le 'username' fait au moins 5 caracteres alphanumérique
-            //  3. vérifier que le password fait au moins 8 caracteres et contient a minima une majuscule/minuscule ainsi qu'un entier (integer)
+    // maintenant que l'on peut afficher nos 2 formulaires l'intéret serait maintenant qu'ils fonctionnent ! pour cela :
+    // FAITE EN SORTE QUE AU CLICK SUR LES BUTTONS POSSEDANT LA CLASS 'square-button' DE :
+    //  1. récuperer la valeur de tout les champs de formulaires
+    //  2. vérifier que le 'username' fait au moins 5 caracteres alphanumérique
+    //  3. vérifier que le password fait au moins 8 caracteres et contient a minima une majuscule/minuscule ainsi qu'un entier (integer)
 
-// Accéder à l'élément form
+    // Accéder à l'élément form
 
     // let email = document.getElementById("data-mail").value;
     // let password = document.getElementById("data-pwd").value;
     // console.log(email);
 
-    // let inputs = document.querySelectorAll('form-control').value;
+    let inputs = document.querySelectorAll('.form-control');
+    // console.log(inputs);
+    let login = document.querySelector('#login');
+    // console.log(login);
+    let signup = document.querySelector('#signup');
 
-    let buttonLogin = document.getElementById('connex-button');
-    let inputEmail = document.getElementById('data-mail');
-    let inputPassword = document.getElementById('data-pwd');
+    login.addEventListener("onclick", function(evenement) { 
 
-    let buttonRegister = document.getElementById('register-button');
-    let input = document.getElementById('pwd');
-    let nom = document.getElementById('mdp');
+            for (let j = 0; j < inputs.length; j++) {
+              document.querySelector('#pwd-1').value;
+            //   document.querySelector('#emai-1').value;
+            if (inputs.type === "text" && inputs.value === "")
+            console.log("it's an empty textfield")
+             
+             
+            }
+            console.log(inputs.value);
+        });
 
-    buttonLogin.onclick = function (){
-        document.getElementById('data-mail').value;
-        document.getElementById('data-pwd').value;
+    
 
-
-        console.log(buttonLogin.value);
-    }
-   
-    buttonRegister.onclick = function (){
-        document.getElementById('pwd').value;
-
-
-        console.log(buttonRegister.value);
-    }
-   
 
 
 
     // --------------------- STEP 3 -------------------------
-        // une fois nos saisies utilisateurs stocker dans des variables faite en sorte de :
-        // A L'INSCRIPTION :
+    // une fois nos saisies utilisateurs stocker dans des variables faite en sorte de :
+    // A L'INSCRIPTION :
 
-            // 1. le code commenter ci-dessous devras etre fonctionnelle (pour ça vous allez devoir déclarer une class 'User' -> POO Javascript)
-                // cette classe devras avoir des les propriétés 'username', 'email', 'password' ainsi qu'une methode nommé 'getUsername' --->
-                // --> qui devra retourner l'username de l'instance courante de 'User'
+    // 1. le code commenter ci-dessous devras etre fonctionnelle (pour ça vous allez devoir déclarer une class 'User' -> POO Javascript)
+    // cette classe devras avoir des les propriétés 'username', 'email', 'password' ainsi qu'une methode nommé 'getUsername' --->
+    // --> qui devra retourner l'username de l'instance courante de 'User'
 
-                // var user = new User('Toto (username)', 'toto@email.fr (email)', 'tamereenslip (password)');
-                // console.log('Bonjour ' + user.getUsername() + ' !');
+    // var user = new User('Toto (username)', 'toto@email.fr (email)', 'tamereenslip (password)');
+    // console.log('Bonjour ' + user.getUsername() + ' !');
 
 
-            // 2. Modifier ensuite le code ci dessus pour qu'a l'instantation d'un nouvelle 'User' ---
-            // --> on utilise les données saisie du formulaire d'inscription pour setup les propriétés notre nouvelle 'User'
-            // puis on stocke ce nouvelle objet utilisateurs dans le 'localStorage' sous la clé 'user'
+    // 2. Modifier ensuite le code ci dessus pour qu'a l'instantation d'un nouvelle 'User' ---
+    // --> on utilise les données saisie du formulaire d'inscription pour setup les propriétés notre nouvelle 'User'
+    // puis on stocke ce nouvelle objet utilisateurs dans le 'localStorage' sous la clé 'user'
 
 
 
 
 
     // --------------------- STEP 4 -------------------------
-        // une fois nos saisies utilisateurs stocker dans des variables faite en sorte de :
-        // A LA CONNEXION :
+    // une fois nos saisies utilisateurs stocker dans des variables faite en sorte de :
+    // A LA CONNEXION :
 
-            // 1. Vérifier l'existance dans le 'localStorage' de la clé 'user'
-                // 1.1 Si la clé 'user' n'existe pas, retourner un message d'erreur a l'utilisateurs (Account do not exist, please register.)
-                // 1.2 Si la clé existe, comparer les données saisie a celle présente dans le 'localStorage'
-                    // 1.2.1 si l'email ou le mot de passe ne correspondent pas, retourner un message d'erreur (les alert() sont proscrit)
+    // 1. Vérifier l'existance dans le 'localStorage' de la clé 'user'
+    // 1.1 Si la clé 'user' n'existe pas, retourner un message d'erreur a l'utilisateurs (Account do not exist, please register.)
+    // 1.2 Si la clé existe, comparer les données saisie a celle présente dans le 'localStorage'
+    // 1.2.1 si l'email ou le mot de passe ne correspondent pas, retourner un message d'erreur (les alert() sont proscrit)
 
-            // 2. Si les données saisies correspondent a celles présentes dans le 'localStorage', rediriger l'utilisateur sur la page 'home.html'
+    // 2. Si les données saisies correspondent a celles présentes dans le 'localStorage', rediriger l'utilisateur sur la page 'home.html'
 
-
+    
+    
 }
